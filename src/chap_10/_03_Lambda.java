fa6c9f1d6e4c1380a6669b71bbbe98189ae47416
Salamamder <mpring@naver.com>
2025-05-27 17:51:19 +0900

    람다식 형태 공부 (전달값1, 전달값2, ...) -> {코드} (): 전달값 1개면 생략 가능 {}: 한 줄 이면 생략가능 + return 없어야 함

---
+++ src/chap_10/_03_Lambda.java
@@ -0,0 +1,28 @@
+package chap_10;
+
+public class _03_Lambda {
+    public static void main(String[] args) {
+        // 람다식
+        // (전달값1, 전달값2, ...) -> { 코드 }
+    }
+
+//     public void print() {
+//        String s = "test";
+//         System.out.println(s);
+//     }
+//    () -> { // print 람다식 변환 완료
+//        String s = "test";
+//        System.out.println(s);
+//    }
+
+//    public void print(String s){
+//        System.out.println(s);
+//    }
+
+//    s -> System.out.println(s)  // 한 줄은 중괄호 없어도 됨 + 한 줄이면 ";" 없애기 : 람다식 완성 ()안에 형식도 쓸 필요 없음, 전달값이 한 개 면"()"도 생략 가능
+
+//    public int add (int x, int y) {
+//        return x + y;
+//    }
+//    (x, y) ->  x + y // return을 쓸려면 {} 써야함
+}
